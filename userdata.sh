d5b4c5005e4562b97e7291a12630228532044cb3
Nouman72884 <noumansatti7@gmail.com>
2020-06-26 23:54:13 +0500

    wordpress app

--- userdata.sh
+++ userdata.sh
@@ -25,6 +25,7 @@ sudo sed -i -e 's/username_here/WordpressUser/g' wordpress/wp-config.php
 sudo sed -i -e 's/password_here/password/g' wordpress/wp-config.php
 sudo sed -i -e 's/localhost/localhost/g' wordpress/wp-config.php
 cd /var/www/html
+sudo rm -r  wordpress-5.4.tar.gz
 sudo rm index.html
-sudo mv /wordpress/* .
+sudo mv wordpress/* .
 sudo systemctl restart apache2
